ef1c4e64fee87c13f183328d502f379437925331
zongzhiping@imassbank.com <zongzhiping@imassbank.com>
2017-05-21 11:28:26 +0800

    1st

---
+++ com.company/src/zzp/geren/Demo.java
@@ -0,0 +1,11 @@
+package zzp.geren;
+
+/**
+ * Created by zzy on 2017/5/21.
+ */
+public class Demo {
+    public static void main(String args[]){
+        System.out.print("hello world!");
+        System.out.print("hello world!");
+    }
+}
